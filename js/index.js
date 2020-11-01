049ce7259f8ed9249b7cc0460dcd6b10bf07c4e7
hellokyla <kylabeals17@gmail.com>
2020-11-01 11:55:30 -0500

    playing with js for dropdown menu

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
 $(".headerMobile").on("click", function(){
     $(".dropDown").slideToggle();
+    // $(".dropDown").css("height", "100vh");
+    $(".dropDown").css("width", "100vw");
+    $(".dropDown").css("background-color", "white");
 })
